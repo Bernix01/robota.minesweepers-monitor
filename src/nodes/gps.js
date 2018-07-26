f9a15b74c9e1970c49a10ae2577aff819805e87a
Guillermo Enrique Bernal Moreira <gbernal096@gmail.com>
2018-07-26 13:18:18 -0500

    zczxc

--- src/nodes/gps.js
+++ src/nodes/gps.js
@@ -18,9 +18,12 @@ const std_msgs = rosnodejs.require("std_msgs").msg;
   io.on("connection", function(socket) {
     console.log("connected", socket.id);
   });
-
-  let sub = nh.subscribe("/chatter", std_msgs.String, data => {
+  var curr_pos = {}
+  let sub = nh.subscribe("/mine", std_msgs.String, data => {
     io.emit("mine", JSON.parse(data.data));
+  });
+  let sub = nh.subscribe("/gps", std_msgs.String, data => {
+    curr_pos = JSON.parse(data.data)
   });
 
   http.listen(port, function() {
