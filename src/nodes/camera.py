274446bec12888747f10c1af804d63e14d21acab
Guillermo Enrique Bernal Moreira <gbernal096@gmail.com>
2018-07-25 23:51:20 -0500

    zxczc

--- src/nodes/camera.py
+++ src/nodes/camera.py
@@ -10,14 +10,16 @@ from std_msgs.msg import String
 def cb(data):
     data = data.data
     camera_img = np.zeros((480,640,3), np.uint8)
-    rospy.loginfo("Hola")
     mine_location_data = json.loads(data)
     mine_location_data = [mine_location_data[i:i + 4] for i in xrange(0, len(mine_location_data), 4)]
     for mine in mine_location_data:
         x, y, w, h = mine
         cv2.rectangle(camera_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    rospy.loginfo("Hola")
     cv2.imshow("camara", camera_img)
+    rospy.loginfo("Hola2")
     cv2.waitKey(500)
+    rospy.loginfo("chao")
 
 def camera():   
     rospy.init_node('camera')
